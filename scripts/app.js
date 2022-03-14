beb95a4689830338f9f5db9d2485e0a1226192c1
Gabriel Martins <gamartins013@gmail.com>
2022-03-13 22:22:18 -0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+
+// metodo de requisição http
+const fetchPokemon =() => {
+    const getPokemonUrl = id =>`http://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    //capturar o array de 150 pokemons em um json
+    for(let i = 1; i <= 150; i++){
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json())) 
+    }
+
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+                accumulator += `
+                    <li class="card ${types[0]}">
+                        <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" />
+                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                        <p class="card-subtitle"> ${types.join(' | ')}</p>
+                    </li>`
+                return accumulator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            //exibir LI no HTML
+            ul.innerHTML = lisPokemons
+        })
+
+
+
+
+
+}
+
+fetchPokemon()

c3c81d452726b3861035a856c83dd6e8f1908234
Gabriel Martins <gamartins013@gmail.com>
2022-03-21 10:11:06 -0300

    change style and colors and poke limit 150 to 251

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,58 +1,62 @@
 const typeColor = {
-    bug:"#26de81",
-    dragon:"#ffeaa7",
-    electric:"#fed330",
-    fairy:"#FF0069",
-    fighting:"#30336b",
-    fire:"#f0932b",
-    flying:"#F5F5F5",
-    grass:"#00b894",
-    ground:"#EFB549",
-    ghost:"#a55eea",
-    ice:"#74b9ff",
-    normal:"#95afc0",
-    poison:"#6c5ce7",
-    psychic:"#a29bfe",
-    rock:"#2d3436",
-    water:"#0190FF",
-    
+    bug: "#91c02d",
+    dragon: "#096cc2",
+    electric: "#f4d23b",
+    fairy: "#ec8fe7",
+    fighting: "#cf3e6a",
+    fire: "#ff9d53",
+    flying: "#95abdd",
+    grass: "#63ba5d",
+    ground: "#d97647",
+    ghost: "#5368ac",
+    ice: "#72ccbf",
+    normal: "#9098a2",
+    poison: "#ac69c7",
+    psychic: "#f87076",
+    rock: "#c6b68d",
+    water: "#4d90d6",
+    dark: "#5b5364",
+    steel: "#5a8fa1"
 };
 
 const url = "https://pokeapi.co/api/v2/pokemon/";
 const card = document.getElementById("card");
-const button = document.getElementById("button");
+const newPoke = document.getElementById("new");
+const forcePoke = document.getElementById("force");
 
-
-// função para gerar um pokemon aleatório entre 1 e 150
-let getPokeData = () =>{
-    let id = Math.floor(Math.random() * 150) + 1;
-    //console.log(id)
-
-    // combinar a url da poke api com o id para gerar o link 
-    const finalUrl = url + id
-
-    // requisição usando fetch
+let getPokeData = () => {
+    let id = Math.floor(Math.random() * 251) + 1;
+    const finalUrl = url + id;
     fetch(finalUrl)
-    .then((response ) => response.json())
-    .then((data) => {
-        gerarCard(data)
-        
-    });
+        .then((response) => response.json())
+        .then((data) => {
+            genCard(data)
+        });
+};
 
+let forcePokeData = () => {
+    const finalUrl = url + document.getElementById("id").value;
+    fetch(finalUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            genCard(data)
+        });
 };
 
 // função para gerar os cards
-    let gerarCard = (data) => {
-        const hp = data.stats[0].base_stat;
-        const imgSrc = data.sprites.other.dream_world.front_default;
-        const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
-        const statAttack = data.stats[1].base_stat;
-        const statDefense = data.stats[2].base_stat;
-        const statSpeed = data.stats[5].base_stat;
+let genCard = (data) => {
+    const hp = data.stats[0].base_stat;
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpAttack = data.stats[3].base_stat;
+    const statSpDefense = data.stats[4].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+    const gameIndex = ('00' + data.id).slice(-3);
 
 
-       
-        card.innerHTML = `
+    card.innerHTML = `
         <p class="hp">
             <span>HP 
             ${hp}
@@ -60,47 +64,57 @@ let getPokeData = () =>{
         </p>
             <img src=${imgSrc} />
             <h2 class="name-pokemon">
-                ${pokeName}
+            #${gameIndex} - ${pokeName}
             </h2>
             <div class="types">
             </div>
             <div class="status">
                 <div>
                     <h3>${statAttack}</h3>
-                    <p>Ataque</p>
+                    <p>ATK</p>
                 </div>
                 <div>
                     <h3>${statDefense}</h3>
-                    <p>Defesa</p>
+                    <p>DEF</p>
+                </div>
+                <div>
+                    <h3>${statSpAttack}</h3>
+                    <p>SP ATK</p>
+                </div>
+                <div>
+                    <h3>${statSpDefense}</h3>
+                    <p>SP DEF</p>
                 </div>
                 <div>
                     <h3>${statSpeed}</h3>
-                    <p>Velocidade</p>
+                    <p>SPD</p>
                 </div>
             </div>
         `;
-        appendTypes(data.types);
+    appendTypes(data.types);
 
-    };
-    
-    let appendTypes = (types) => {
-        if (types.length === 2) {
-            console.log("2");
-            card.style.background = `radial-gradient(circle at 50% 0%, ${typeColor[types[1].type.name]}, ${typeColor[types[0].type.name]} 36%, #ffffff 36%)`;
-        }
-        else {
-            console.log("1");
-            card.style.background = `radial-gradient(circle at 50% 0%, ${typeColor[types[0].type.name]}, ${typeColor[types[0].type.name]} 36%, #ffffff 36%)`;
-        }
-        types.forEach((item) => {
-            let span = document.createElement("SPAN")
-            span.textContent = item.type.name;
-            document.querySelector(".types").appendChild(span);
-            span.style.backgroundColor = typeColor[item.type.name];
-        });
-    };
+};
 
-// adicionando evento de click no botão de novo pokemon
-button.addEventListener("click", getPokeData);
+let appendTypes = (types) => {
+    if (types.length === 2) {
+        card.style.background = `radial-gradient(circle at 50% 0%, transparent, transparent 36%, white 36.1%), linear-gradient(115deg, ${typeColor[types[0].type.name]}, ${typeColor[types[1].type.name]} 66%)`;
+    } else {
+        card.style.background = `radial-gradient(circle at 50% 0%,${typeColor[types[0].type.name]} 36%, white 36.1%)`;
+    }
+    types.forEach((item) => {
+        let span = document.createElement("SPAN")
+        span.textContent = item.type.name;
+        document.querySelector(".types").appendChild(span);
+        span.style.backgroundColor = typeColor[item.type.name];
+    });
+};
 
+newPoke.addEventListener("click", getPokeData);
 window.addEventListener("load", getPokeData);
+forcePoke.addEventListener("click", forcePokeData);
+document.getElementById("id").defaultValue = 1;
+document.getElementById("id").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        forcePokeData();
+    }
+});
